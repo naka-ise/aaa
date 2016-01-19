78fdfa649264d0c1fe446ddfa4db49c0c774a4df
naka <btf@gmail.com>
2016-01-19 16:50:17 +0200

    DB setup through Model commit

--- WpfApplication1/Model/MyModel.cs
+++ WpfApplication1/Model/MyModel.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 using System.Data.Sql;
 using System.Data.SqlClient;
 using System.Data;
+using System.IO;
 
 namespace WpfApplication1.Model
 {
-    class MyModel: IModel
+    class MyModel : IModel
     {
-        public static string connection = "Data Source=(LocalDB)\\v11.0;AttachDbFileName=\"C:\\Hallel\\Presonal\\TeachMe\\WpfApplication1\\Model\\TeachMe_DB.mdf\";Integrated Security=True";
-        public static SqlConnection SqlCon= new SqlConnection(connection);
-        static int LessonID=1;
+        public static string directoryPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\Model\\TeachMe_DB.mdf";
+        public static string connection = "Data Source=(LocalDB)\\v11.0;AttachDbFileName=" + directoryPath + ";Integrated Security=True";
+        public static SqlConnection SqlCon = new SqlConnection(connection);
+        static int LessonID = 1;
 
 
         public void AddUser(string[] details, string user)
@@ -24,7 +26,7 @@ namespace WpfApplication1.Model
                 SqlCon.Open();
                 string query;
                 SqlCommand command;
-                
+
                 if (user == "Student")
                 {
                     query = "INSERT INTO dbo.Students(Id, F_Name, L_Name, City, Phone, Email, AcademicDegree)  VALUES (@Id, @F_Name, @L_Name, @City, @Phone, @Email, @AcademicDegree)";
@@ -65,42 +67,42 @@ namespace WpfApplication1.Model
             }
         }
 
-        
+
         public void DeleteUser(string[] details, string user)
         {
             try
             {
                 SqlCon.Open();
-        
+
                 string query;
                 SqlCommand command;
-        
+
                 if (user == "teacher")
                 {
                     query = "DELETE *    FROM Teacher    Where Id=@Id";
                 }
-        
+
                 else
                 {
                     query = "DELETE *    FROM Students    Where Id=@Id";
                 }
-        
+
                 command = new SqlCommand(query, SqlCon);
                 command.Parameters.Add("@Id", details[0]);
                 command.ExecuteNonQuery();
             }
-        
+
             catch (Exception ex)
             {
                 throw ex;
             }
-        
+
             finally
             {
                 SqlCon.Close();
             }
         }
-        
+
         /*public void UpdateTeacher(string[] details)
         {
             try
@@ -133,7 +135,7 @@ namespace WpfApplication1.Model
             {
                 SqlCon.Open();
 
-                string query = "SELECT T.Id, T.F_Name, T.L_Name, T.City, T.Phone, T.Email, T.AcademicDegree   FROM Teacher T INNER JOIN TeacherProfessions TP ON T.Id=TP.TeacherID INNER JOIN Professions P ON TP.ProfessionCode=P.ProfessionCode   WHERE  P.ProfessionName=" + details[0] +  "AND TP. T.City=" + details[1] + "AND TP.HourPrice<=" + details[2];
+                string query = "SELECT T.Id, T.F_Name, T.L_Name, T.City, T.Phone, T.Email, T.AcademicDegree   FROM Teacher T INNER JOIN TeacherProfessions TP ON T.Id=TP.TeacherID INNER JOIN Professions P ON TP.ProfessionCode=P.ProfessionCode   WHERE  P.ProfessionName=" + details[0] + "AND TP. T.City=" + details[1] + "AND TP.HourPrice<=" + details[2];
 
                 SqlCommand command = new SqlCommand(query, SqlCon);
                 SqlDataAdapter tableAdapter = new SqlDataAdapter(command);
@@ -152,14 +154,15 @@ namespace WpfApplication1.Model
                 SqlCon.Close();
             }
         }
-        
-        public void SetNewLesson(string[] details){
+
+        public void SetNewLesson(string[] details)
+        {
             try
             {
                 SqlCon.Open();
 
                 string query = "INSERT INTO StudentsLessons (LessonID, StudentID, TeacherID, ProfessionCode, Date, Hour, AssingmentCode, Duration " +
-                                "VALUES(" + LessonID.ToString() +", SELECT Id FROM Students WHERE ID=@StudentID, SELECT Id FROM Teacher WHERE T.Id=@TeacherID, SELECT ProfessionCode FROM Professions WHERE ProfessionName=@ProfessionName, @Date, @Hour, NULL, @Duration";
+                                "VALUES(" + LessonID.ToString() + ", SELECT Id FROM Students WHERE ID=@StudentID, SELECT Id FROM Teacher WHERE T.Id=@TeacherID, SELECT ProfessionCode FROM Professions WHERE ProfessionName=@ProfessionName, @Date, @Hour, NULL, @Duration";
                 SqlCommand command = new SqlCommand(query, SqlCon);
                 command.Parameters.Add("@StudentID", details[0]);
                 command.Parameters.Add("@TeacherID", details[1]);
@@ -215,7 +218,7 @@ namespace WpfApplication1.Model
                 SqlCon.Close();
             }
         }
-        
+
         public void PayForLesson(string[] details)
         {
             try
